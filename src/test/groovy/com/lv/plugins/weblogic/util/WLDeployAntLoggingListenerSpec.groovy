2aabce02e3a1cb9941740ce7cb4b03c1e2a2919d
Williams, Sion <sion.williams@lv.com>
2014-09-30 16:58:08 +0100

    Added an Ant logging listener as per Benjamin's example in the Cargo plugin. https://github.com/bmuschko/gradle-cargo-plugin/blob/master/src/test/groovy/org/gradle/api/plugins/cargo/util/CargoAntLoggingListenerSpec.groovy

---
+++ src/test/groovy/com/lv/plugins/weblogic/util/WLDeployAntLoggingListenerSpec.groovy
@@ -0,0 +1,71 @@
+package com.lv.plugins.weblogic.util
+
+import com.lv.plugins.weblogic.tasks.WLDeployTask
+import org.apache.tools.ant.BuildEvent
+import org.apache.tools.ant.BuildListener
+import org.apache.tools.ant.Project
+import org.apache.tools.ant.taskdefs.Copy
+import org.gradle.api.Task
+import org.gradle.api.logging.Logger
+import spock.lang.Specification
+
+/**
+ * Cargo Ant build listener unit tests.
+ *
+ * @author Benjamin Muschko
+ * @author Sion Williams
+ */
+class WLDeployAntLoggingListenerSpec extends Specification {
+    BuildListener buildListener
+
+    def setup() {
+        buildListener = new WLDeployAntLoggingListener()
+    }
+
+    void 'Message not logged for non-WLDeploy Ant task'() {
+        given:
+            Task antCopyTask = new Copy()
+            BuildEvent buildEvent = new BuildEvent( antCopyTask )
+            buildEvent.message = 'Weblogic started'
+            Logger mockLogger = Mock( Logger )
+            buildListener.logger = mockLogger
+
+        when:
+            buildListener.messageLogged( buildEvent )
+
+        then:
+            0 * mockLogger.lifecycle('Weblogic started')
+    }
+
+    void 'Message not logged for WLDeploy Ant task with debug priority'() {
+        given:
+            Task antWLDeployTask = new WLDeployTask()
+            BuildEvent buildEvent = new BuildEvent( antWLDeployTask )
+            buildEvent.message = 'Weblogic started'
+            buildEvent.priority = Project.MSG_DEBUG
+            Logger mockLogger = Mock( Logger )
+            buildListener.logger = mockLogger
+
+        when:
+            buildListener.messageLogged( buildEvent )
+
+        then:
+            0 * mockLogger.lifecycle('Weblogic started')
+    }
+
+    void 'Message is logged for WLDeploy Ant task with info priority'() {
+        given:
+            Task antWLDeployTask = new WLDeployTask()
+            BuildEvent buildEvent = new BuildEvent( antWLDeployTask )
+            buildEvent.message = 'Weblogic started'
+            buildEvent.priority = Project.MSG_INFO
+            Logger mockLogger = Mock( Logger )
+            buildListener.logger = mockLogger
+
+        when:
+            buildListener.messageLogged( buildEvent )
+
+        then:
+            0 * mockLogger.lifecycle('Weblogic started')
+    }
+}
